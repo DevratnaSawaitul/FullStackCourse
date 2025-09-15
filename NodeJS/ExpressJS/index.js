99d544c73ca7ba2875a283f73a4ff2870032e8df
Devratna Sawaitul <devratnasawaitul6@gmail.com>
2025-09-15 07:50:23 +0530

    middleware

--- NodeJS/ExpressJS/index.js
+++ NodeJS/ExpressJS/index.js
@@ -13,11 +13,42 @@ app.get("/file", (req, res) => {
 });
 
 // passing data in files
-app.set('view engine', 'pug')
-app.get('/data', (req, res) => {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
-})
+app.set("view engine", "pug");
+app.get("/data", (req, res) => {
+  res.render("index", { title: "Hey", message: "Hello there!" });
+});
 
 app.listen(port, () => {
   console.log("app is active and listening");
+});
+
+// middle wear use this we can before execution of getter block
+const outMiddleWear = (res, req, next) => {
+  console.log("we are in middle wear");
+  next();
+};
+
+app.get("/middlewearuse", outMiddleWear, (req, res) => {
+  res.send("this is middlewear getter");
+});
+
+// static files like we used css in html is not allowed in express so we used express.static to add propeties
+app.use(express.static("public")); // defined folder where static files will be added in application
+
+// using query params
+app.get("/queryExample", (req, res) => {
+  console.log(req.query);
+  let str = "";
+  // if gender filter in applied we will show it is res
+  if (req.query.gender != null) {
+    str = `this is gender for: ${req.query.gender}`;
+  } else {
+    str = "there is no gender";
+  }
+  res.send(str);
+});
+
+// string param example
+app.get('/user/:username', (req, res) => {
+  res.send(`this is username ${req.params.username}`);
 });
